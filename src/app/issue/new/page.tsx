bd383b123d4762ab5f29313c87d2895a5ed134a3
Daniel <truelovee0112@outlook.com>
2025-02-08 18:16:22 -0800

    add router

--- src/app/issue/new/page.tsx
+++ src/app/issue/new/page.tsx
@@ -1,16 +1,32 @@
 'use client';
 import { Button, Text, TextField } from '@radix-ui/themes'
 import React from "react";
-import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
+import { useForm, Controller } from 'react-hook-form';
+import { useRouter } from 'next/navigation';
+import SimpleMDE from 'react-simplemde-editor';
+import axios from 'axios';
 
+interface IssueForm  {
+  title: string,
+  description: string,
+}
 const NewIssue = () => {
+  const {register, control, handleSubmit} = useForm<IssueForm>();
+  const router = useRouter();
   return (
-    <div className='space-y-6'>
-        <TextField.Root placeholder='text...'/>
-        <SimpleMDE placeholder='description...'/>
+    <form className='space-y-6' onSubmit={handleSubmit(async (data) => {
+      await axios.post('/api/issues', data);
+      router.push('/issue');
+    })}>
+        <TextField.Root placeholder='text...' {...register('title')}/>
+        <Controller 
+        name='description'
+        control={control}
+        render={({field})=><SimpleMDE placeholder='description...' {...field}/>}/>
+
         <Button>Create Issue</Button>
-    </div>
+    </form>
   )
 }
 
